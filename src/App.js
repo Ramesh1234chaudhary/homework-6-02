dfbbc87be3b23f597ea97cbf9032a892513ffead
ramesh <romanchaudhary01994@gmail.com>
2023-04-28 17:28:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router, Routes ,Route} from "react-router-dom"
+import Header from "./Components/Header"
+import Home from "./Components/Home"
+import Footer from "./Components/Footer"
+
+import "./Styles/App.scss"
+import "./Styles/Header.scss"
+import "./Styles/Home.scss"
+import "./Styles/Footer.scss"
+function App() {
+  return (
+   <Router>
+    <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+    <Footer></Footer>
+   </Router>
+  );
+}
+export default App;
---
+++ src/Components/imges.js
@@ -0,0 +1,3 @@
+export const bg='https://raw.githubusercontent.com/Awesh7860/React-homework6/master/src/Resources/bg.avif'
+export const bg1='https://raw.githubusercontent.com/Awesh7860/React-homework6/master/src/Resources/bg1.avif'
+ 
